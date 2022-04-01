e151e8ace9a006f2781591bc817668acf33e583c
mohamedamine-tarhouni <mohamedamine.tarhouni@ynov.com>
2022-04-02 01:17:09 +0200

    Projet debut

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,7 @@
-import 'package:firebase_core/firebase_core.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:chatbox/views/sigin.dart';
 import 'package:flutter/material.dart';
 
-
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  //await Firebase.initializeApp();
+void main() {
   runApp(const MyApp());
 }
 
@@ -16,103 +12,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.red,
-      ),
-      home: const MyHomePage(title: 'CleMidCher Forum'),
+        title: 'FlutterChat',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+        primaryColor: Color(0xff145C9E),
+    scaffoldBackgroundColor: Color(0xff1F1F1F),
+    accentColor: Color(0xff007EF4),
+    fontFamily: "OverpassRegular",
+    visualDensity: VisualDensity.adaptivePlatformDensity,
+    ),
+      home:SignIn()
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-  double slider = 5.5;
-  late String mail;
-  late String password;
-  @override
-  void initState() {
-    super.initState();
-    upload();
-  }
-
-  Future upload() async {
-    /// Initialize Firebase
-    await Firebase.initializeApp();
-
-
-    /// Upload document to firestore
-    final refUser = FirebaseFirestore.instance.collection('Users').doc();
-    await refUser.set({'username': 'tarhouni'});
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Stack(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: NetworkImage(
-                    "https://mfiles.alphacoders.com/965/thumb-1920-965175.jpg"),
-                fit: BoxFit.fill,
-              ),
-            ),
-            child: null /* add child content here */,
-          ),
-          Container(
-            child: bodyPage(),
-            padding: EdgeInsets.all(20),
-          ),
-        ],
-      ),
-    );
-  }
-
-  Widget bodyPage() {
-    return Column(
-      children: [
-        SizedBox(
-          height: 100,
-        ),
-        TextField(
-          decoration: InputDecoration(
-              filled: true,
-              fillColor: Colors.white,
-              hintText: "Entrer votre mail",
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(20))),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-        TextField(
-          decoration: InputDecoration(
-              filled: true,
-              fillColor: Colors.white,
-              hintText: "Entrer votre mot de passe",
-              border:
-                  OutlineInputBorder(borderRadius: BorderRadius.circular(20))),
-        ),
-        SizedBox(
-          height: 10,
-        ),
-      ],
-    );
-  }
-}
---
+++ lib/widgets/widget.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+@override
+Widget appBarMain(BuildContext context) {
+  return AppBar(
+    title: Image.asset(
+      "assets/images/logo.png",
+      height: 40,
+    ),
+    elevation: 0.0,
+    centerTitle: false,
+  );
+}
