e069ccad08b1398bfccd092d8fa02f6958da599a
mohamedamine-tarhouni <mohamedamine.tarhouni@ynov.com>
2022-04-06 22:45:39 +0200

    connexion inscription en cours

--- lib/views/sigin.dart
+++ lib/views/sigin.dart
@@ -35,7 +35,7 @@ class _SignInState extends State<SignIn> {
       });
       databaseMethods.getUserByUseremail(emailTextEditingController.text).then((value){
         snapshotUserInfo=value;
-        HelperFunctions.saveUserNameSharedPreference(snapshotUserInfo?.docs[0].data()["name"]);
+        HelperFunctions.saveUserEmailSharedPreference(snapshotUserInfo?.docs[0].data()["name"]);
 
       });
       authMethods.signInWithEmailAndPassowrd(
